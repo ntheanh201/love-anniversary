3454f1408e8fab6cf7b94e38ee358fdcee4f49eb
ntheanh201 <ntheanh201@gmail.com>
2018-04-16 02:08:47 +0700

    a

--- js/countup.js
+++ js/countup.js
@@ -4,11 +4,8 @@ function aaa()
 {   
 
 var now = new Date(); 
-<<<<<<< HEAD
 var xmasDay = new Date(1999, 01, 20,00,0,0); // khởi tạo ngày Noel cùng năm với ngày hiện tại 
-=======
 var xmasDay = new Date(0000, 00, 00,00,0,0); // khởi tạo ngày Noel cùng năm với ngày hiện tại 
->>>>>>> 982d9f701a3976a08812673a8cc73bb884e6d66e
 var offset = -xmasDay.getTime() + now.getTime(); // lấy độ lệch của 2 mốc thời gian, đơn vị tính là millisecond 
 var totalDays = Math.round(offset / 1000 / 60 / 60 / 24); 
 var totalHours = Math.round(offset / 1000 / 60 / 60); 
